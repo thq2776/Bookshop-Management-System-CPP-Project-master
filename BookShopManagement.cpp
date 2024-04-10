8b780e9c6b12fdd95fb8d3244519ccbc8dd4f911
Trần Ngọc Thanh <56815987+thq2776@users.noreply.github.com>
2024-04-10 13:03:50 +0700

    Update BookShopManagement.cpp

--- BookShopManagement.cpp
+++ BookShopManagement.cpp
@@ -275,6 +275,316 @@ void books::display()
         cout << endl << endl << endl << endl ;
     }
 }
+
+// ---------------
+// class suppliers
+// ---------------
+
+void suppliers::add_sup()
+{
+    cout << "Enter the Supplier Name : ";
+    cin >> name;
+    cout << "Enter Phone no. : ";
+    cin >> phn;
+    cout << "Enter the address (in 3 lines) : ";
+    cin >> addr_line1;
+    cin >> addr_line2;
+    cin >> addr_city;
+    cout << "Enter State : ";
+    cin >> addr_state;
+    stmt.str("");
+    stmt << "Insert into suppliers(name,phone_no,addr1,addr2,addr_city,addr_stat) values('" << name << "'," << phn << ",'" << addr_line1 << "','" << addr_line2 << "','" << addr_city << "','" << addr_state << "');";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if (!(res_set))
+        cout << endl << endl << "Supplier Record Inserted Successfully" << endl << endl << endl;
+    else
+        cout << endl << endl << "Entry ERROR !" << endl << "Contact Technical Team " << endl << endl << endl;
+}
+
+void suppliers::remove_supplier()
+{
+    cout << "Enter the supplier id to remove the Supplier : ";
+    cin >>  id;
+    stmt.str("");
+    stmt << "Delete from suppliers where id = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << "Supplier Removed.";
+}
+
+void suppliers::search_id()
+{
+    cout << "Enter the supplier id to find the Supplier details : ";
+    cin >>  id;
+    stmt.str("");
+    stmt << "Select * from suppliers where id = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if((row = mysql_fetch_row(res_set)) != NULL)
+    {
+        cout << "Details of Supplier Id : " << row[0] << endl;
+        cout << "Name : " << row[1] << endl;
+        cout << "Phone no. : " << row[2] << endl;
+        cout << "Address Line 1 : " << row[3] << endl;
+        cout << "Address Line 2 : " << row[4] << endl;
+        cout << "City : " << row[5] << endl;
+        cout << "State : " << row[6] << endl;
+    }
+    else
+    {
+        cout << "No Supplier Found!!";
+    }
+}
+
+
+// ------------------
+// class Purchases
+// ------------------
+
+void purchases::new_ord()
+{
+    cout << "Enter the book Id : ";
+    cin >> book_id;
+    cout << "Enter Supplier Id : ";
+    cin >> sup_id;
+    cout << "Enter the Quantity : ";
+    cin >> qty;
+    cout << "Estimated expected Delivery (in days) : ";
+    cin >> eta;
+    stmt.str("");
+    stmt << "Insert into purchases (book_id,sup_id,qty,dt_ord,eta) values ( " << book_id << "," << sup_id << "," << qty << ",curdate(),Date_add(curdate(), INTERVAL " << eta << " DAY)" << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << "New order Added!!";
+}
+
+void purchases::mark_reciv()
+{
+    cout << "Enter the order id for order recieved : ";
+    cin >> ord_id;
+    stmt.str("");
+    stmt << "Update puchases set recieved = 'T' where ord_id = " << ord_id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << "Recieved Marked successfully";
+}
+
+void purchases::mar_cancel()
+{
+    cout << "Enter the order id for order cancelled : ";
+    cin >> ord_id;
+    stmt.str("");
+    stmt << "Update puchases set recieved = 'C' where ord_id = " << ord_id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << "Cancelled Marked successfully";
+}
+
+void purchases::view()
+{
+    int c;
+    cout << "Select an Option" << endl;
+    cout << "1. View orders not Recieved" << endl;
+    cout << "2. View orders Cancelled" << endl;
+    cout << "3. View orders Recieved" << endl;
+    cout << "Enter Your choice : ";
+    cin >> c;
+    if (c == 1)
+        received = 'F';
+    else if (c == 2)
+        received = 'C';
+    else if (c == 3)
+        received = 'T';
+    else
+        return;
+    stmt.str("");
+    stmt << "Select * from purchases where recieved = '" << received << "';";
+    query  = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if (c == 1)
+        cout << endl << "Orders not recieved are" << endl;
+    else if (c == 2)
+        cout << endl << "Orders Cancelled are" << endl;
+    else if (c == 3)
+        cout << endl << "Orders recieved are" << endl;
+    while((row = mysql_fetch_row(res_set)) != NULL)
+    {
+        cout << endl;
+        cout << "Order Id : " << row[0] << endl;
+        cout << "Book Id : " << row[1] << endl;
+        cout << "Supplier Id :" << row[2] << endl ;
+        cout << "Quantity : " << row[3] << endl;
+        cout << "Date Ordered : " << row[4] << endl;
+        cout << "Estimated Delivery date : " << row[5] << endl;
+        cout << endl << endl << endl;
+    }
+    cout <<endl << endl << endl <<endl << endl;
+}
+
+// ----------------
+// class employees
+// ----------------
+
+void employees::add_emp()
+{
+    cout << "Enter Your Id for verification : ";
+    cin >> id ;
+    stmt.str("");
+    stmt << "Select mgr_stat from employees where id  = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if ((row = mysql_fetch_row(res_set)) == NULL)
+    {
+        cout << "Employee Not Found!!" << endl << endl << endl;
+        return;
+    }
+    else
+    {
+        mgr_status = (char*) row[0];
+        if (mgr_status == "T")
+        {
+            cout << "You Do Not have Manager Rights!!!" << endl << endl ;
+            return;
+        }
+    }
+    cout << "Enter The details of new employees!!";
+    cout << "Enter The name of the employee : ";
+    cin >> name;
+    cout << "Enter the Address (in 3 lines) : " << endl;
+    cin >> addr_line1;
+    cin >> addr_line2;
+    cin >> addr_city;
+    cout << "Enter State : ";
+    cin >> addr_state;
+    cout << "Enter phone no.";
+    cin >> phn;
+    cout << "Enter the salary : ";
+    cin >> salary;
+    stmt.str("");
+    stmt << "Insert into employees (name,addr1,addr2,addr_city,addr_stat,phone_no,doj,salary) values ('" << name << "','" << addr_line1 << "','" << addr_line2 << "','" << addr_city << "','" << addr_state << "'," << phn << ",curdate()," << salary << ");";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << endl << endl << "Employee Added Succesfully!" << endl << endl << endl;
+}
+
+void employees::assign_mgr_stat()
+{
+    cout << "Enter Your Id for verification : ";
+    cin >> id ;
+    stmt.str("");
+    stmt << "Select mgr_stat from employees where id  = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if ((row = mysql_fetch_row(res_set)) == NULL)
+    {
+        cout << "Employee Not Found!!" << endl << endl << endl;
+        return;
+    }
+    else
+    {
+        mgr_status = (char*) row[0];
+        if (mgr_status == "T")
+        {
+            cout << "You Do Not have Manager Rights!!!" << endl << endl ;
+            return;
+        }
+    }
+    cout << "Enter the employee id to grant Manager status : ";
+    cin >> id;
+    stmt.str("");
+    stmt << "update employees set mgr_stat = 'T' where id = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << endl << endl << endl;
+    cout << "Manager Status granted";
+    cout << endl << endl << endl;
+}
+
+void employees::search_emp()
+{
+    cout << "Enter the id for searching an employee : ";
+    cin >> id ;
+    stmt.str("");
+    stmt << "Select * from employees where id  = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if ((row = mysql_fetch_row(res_set)) != NULL)
+    {
+        cout << "Employees Details" << endl ;
+        cout << "Name : " << row[1] <<endl;
+        cout << "Address : " << endl << row[2] << endl << row[3] << endl << row[4] << endl;
+        cout << "State : " << row[5] << endl;
+        cout << "Contact no. : " << row[6] << endl;
+        cout << "Date of Joining" << row[7] << endl;
+        cout << "Salary : " << row[8] << endl << endl << endl;
+    }
+    else
+    {
+        cout << "No Employee Found!!" << endl << endl << endl;
+    }
+}
+
+void employees::display()
+{
+    int i = 0;
+    query = "Select * from employees;";
+    q = query.c_str();
+    mysql_query(conn,q);
+    res_set = mysql_store_result(conn);
+    if((row = mysql_fetch_row(res_set)) != NULL)
+    {
+       do
+       {
+        cout << "Employees Details of Emp no." << ++i << endl ;
+        cout << "Name : " << row[1] <<endl;
+        cout << "Address : " << endl << row[2] << endl << row[3] << endl << row[4] << endl;
+        cout << "State : " << row[5] << endl;
+        cout << "Contact no. : " << row[6] << endl;
+        cout << "Date of Joining" << row[7] << endl;
+        cout << "Salary : " << row[8] << endl;
+        cout << endl << endl << endl << endl;
+       }while((row = mysql_fetch_row(res_set)) != NULL);
+    }
+    else
+    {
+        cout << "Employees Not found!" << endl;
+    }
+}
+
+void employees::update_sal()
+{
+    cout << "Enter the id to update the salary of an employee : ";
+    cin >> id ;
+    cout << "Enter the updated salary : ";
+    cin >> salary;
+    stmt.str("");
+    stmt << "Update employees set salary = " << salary << " where id  = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn,q);
+    cout << endl << endl << endl << endl;
+    cout << "Salary update Successfully";
+    cout << endl << endl << endl << endl;
+}
 // +----------------------------------+
 // |		Main Function 	      |
 // +----------------------------------+
